5c75772199b7e7d2d9ae249637153b56a9c61165
Niels Gerritsen <n.gerritsen@coolblue.nl>
2016-04-21 00:13:07 +0200

    First commit

---
+++ src/dots-n-boxes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Game from './game'
+
+const DotsNBoxes = () => (
+  <div>
+    <h1>DotsNBoxes</h1>
+    <Game/>
+  </div>
+)
+
+export default DotsNBoxes
---
+++ src/game.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Board from './board'
+import Score from './score'
+
+const Game = () => (
+  <div>
+    <Board/>
+    <Score/>
+  </div>
+)
+
+export default Game
