1aa42d7fd3edc60e6add5b5d9a8ea329407493b2
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-04-22 15:58:52 +0200

    Calculate next player correctly

--- src/helpers/game.js
+++ src/helpers/game.js
@@ -1,16 +1,39 @@
 import { default as createBoard } from '../factories/board'
 import { calculateScore } from '../helpers/score'
 
-export function calculateGameState ({ width, height, moves }) {
-  const activePlayer = moves.length % 2
-  const board = createBoard(width, height, moves)
-  const { scorePlayer0, scorePlayer1 } = calculateScore(board)
+const initialScore = { scorePlayer0: 0, scorePlayer1: 0 }
 
-  return {
+export function calculateGameState (state, move = 0, previousPlayer = 0, previousScore = initialScore) {
+  const { width, height, moves } = state
+  const movesToCalculate = moves.slice(0, move)
+  const board = createBoard(width, height, movesToCalculate)
+  const score = calculateScore(board)
+  const { scorePlayer0, scorePlayer1 } = score
+  const activePlayer = determineNextPlayer(previousPlayer, previousScore, score)
+
+  const gameState = {
     activePlayer,
     board,
     width,
     scorePlayer0,
     scorePlayer1
   }
+
+  if (moves.length === move) {
+    return gameState
+  }
+
+  return calculateGameState(state, move + 1, activePlayer, score)
+}
+
+function determineNextPlayer (previousPlayer, previousScore, currentScore) {
+  const previousPlayerPreviousScore = previousScore['scorePlayer' + previousPlayer]
+  const previousPlayerCurrentScore = currentScore['scorePlayer' + previousPlayer]
+  const previousPlayerScored = previousPlayerCurrentScore > previousPlayerPreviousScore
+
+  if (previousPlayerScored) {
+    return previousPlayer
+  }
+
+  return previousPlayer === 0 ? 1 : 0
 }
